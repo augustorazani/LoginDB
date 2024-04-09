8452d9be7404fa1035313cdf8820838c3cff5775
Augusto Mattevi <razanidark@gmail.com>
2024-04-09 11:20:39 -0300

    a

--- LoginDB/Propriedades.cs
+++ LoginDB/Propriedades.cs
@@ -8,5 +8,19 @@ namespace LoginDB
 {
     internal class Propriedades
     {
+        public int IdLogin { get; private set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public Propriedades(int idLogin, string nome, string senha)
+        {
+            IdLogin = idLogin;
+            Nome = nome;
+            Senha = senha;
+        }
+        public Propriedades()
+        {
+        }
+
+
     }
 }
